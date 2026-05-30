3311c1f89335b4381da9ec3c743029e7fb564484
alonemarka <alonemarkaafk@gmail.com>
2026-05-31 02:35:20 +0300

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8597974338:AAEiJvJrHY-Ae4HYAZWoZky-31axSuppS9I")
+ADMIN_IDS = [8773299135, 8973632679, 8230461239, 6318435017]   # Kendi ID'lerini yaz
+
+COLORS = {
+    "light": {"bg": "#f8f9fa", "text": "#1f1f1f", "accent": "#0d6efd"},
+    "dark": {"bg": "#1e1e1e", "text": "#ffffff", "accent": "#0dcaf0"},
+    "pink": {"bg": "#fff0f5", "text": "#4a0033", "accent": "#ff69b4"},
+    "blue": {"bg": "#e6f0ff", "text": "#002b4a", "accent": "#3498db"},
+}
