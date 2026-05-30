7a8acf16f31fbb925b12ddf7248db4bb081d90b8
alonemarka <alonemarkaafk@gmail.com>
2026-05-31 02:41:24 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8597974338:AAEiJvJrHY-Ae4HYAZWoZky-31axSuppS9I")
-ADMIN_IDS = [8773299135, 8973632679, 8230461239, 6318435017]   # Kendi ID'lerini yaz
+# ✅ DOĞRU ŞEKİL
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Admin ID'lerin (istediğin kadar ekleyebilirsin)
+ADMIN_IDS = [8773299135, 8973632679, 8230461239, 6318435017]
 
 COLORS = {
     "light": {"bg": "#f8f9fa", "text": "#1f1f1f", "accent": "#0d6efd"},
